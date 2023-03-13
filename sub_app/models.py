8a4229775d89481336b639343b825ddd6cbae906
AbdurrehmanM018 <zamancheema037@gmail.com>
2023-03-13 13:14:16 -0400

    file was deleted

--- sub_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.db import models
-
-
-#django model.
-class SkinModel(models.Model):
-	pic=models.ImageField(upload_to='images',blank=True)
-	url=models.CharField(max_length=100,blank=True)
-
